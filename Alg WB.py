e4f39b5d1c4cd563eddb0e018fe9695a59057d15
Kyle <kjwalker@my.ccri.edu>
2022-09-26 19:31:53 -0400

    Testing Commit

---
+++ Alg WB.py
@@ -0,0 +1,50 @@
+#Collect users height
+
+#Assign to variable height
+#height= float(input('What is your height in feet?'))
+#Dislay height
+#print('Your height is', height ,'feet')
+
+#Question 2
+#color=input('What is your favorite color?')
+#
+#Print('Your favorite color is', color, "!")
+
+#Question 3
+# a. b=2+a
+# b. a=b*4
+# c. b=a/3.14
+# d. a=b-8
+
+#Question 4
+# w=5
+# x=4
+# y=8
+# z=2
+
+# result = x+y
+
+# Question 5
+#total = 10.1+14
+#print(type(total))
+
+# Question 6
+#due= toal- down_payment
+
+#Question 7
+#total=subtotal * 0.15
+
+#Question 8
+#a=5
+#b=2
+#c=3
+#result= a+b*c
+#print(resulty)
+
+#Question 9
+# num=99
+# num=67
+# num=5
+# print(num)
+
+# Question 10
